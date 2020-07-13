a4271ecea20f9b0be283c24b771906a54ce4d8d0
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-07-12 22:16:57 -0400

    working through problems

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,14 +11,17 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
-print (y)
+for num in range(1,6):
+    if num >=1 and num <=5:
+        y.append(num)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for num in range(10):
+    y.append(num**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -26,7 +29,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [element.capitalize() for element in a]
 
 print(y)
 
